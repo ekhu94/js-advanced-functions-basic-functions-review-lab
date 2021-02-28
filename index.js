8b013242701f28f14fd9145578d93ec738ebd300
Erik Huang <erikhuangdrums@gmail.com>
2021-02-27 21:25:06 -0800

    done

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Your code here
+const saturdayFun = (n="roller-skate") => {
+    return `This Saturday, I want to ${n}!`
+}
+
+const mondayWork = (n="go to the office") => `This Monday, I will ${n}.`
+
+const wrapAdjective = (style="*") => {
+    return (adj="||") => `You are ${style}${adj}${style}!`
+}
+
+const Calculator = {
+    add: function(x, y) {return x + y},
+    subtract: function(x, y) {return x - y},
+    multiply: function(x, y) {return x * y},
+    divide: function(x, y) {return x / y}
+};
+
+const actionApplyer = (n, arr) => {
+    if (arr.length === 0) return n;
+    let a = n;
+    for (let num of arr) {
+        a = num(a)
+    }
+    return a
+}
